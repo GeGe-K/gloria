8184b84f9a21c3d6aaf2865b2bb3162e8a5bc9c6
Gloria Givondo <gloriagivondo@gmail.com>
2019-08-11 18:08:24 +0300

    This function enables an item on the menu to be clicked. On click the navbar closes and is directed that section on the page.

--- js/script.js
+++ js/script.js
@@ -27,5 +27,33 @@
     function o(t) {
         t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
     }
+
+    // navigation
+
+    var app = function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.nav__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener('click', function() {
+                return toggleClass(body, 'nav-active');
+            });
+            menuItems.forEach(element => {
+                element.addEventListener('click', function () {
+                    return toggleClass(body, 'nv-active');
+                });
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass)) element.classList.remove(stringClass);
+        };
+        init();
+    }();
 }
 )
